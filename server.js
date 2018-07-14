345f10e39244cb2131d1ede218e7faf4f45a3dc5
hvoncannon <hayden.voncannon@gmail.com>
2018-07-14 10:47:35 -0700

    added api route and reservation object array

--- server.js
+++ server.js
@@ -7,6 +7,27 @@ var path = require('path');
 var app = express();
 var PORT = 3000;
 
+//set up the body parser
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
+//reservation array
+var reservations = [
+    {
+        name: "hayden",
+        phoneNumber: 9499999999,
+        email: "hayden@gmail.com",
+        uniqueID: "rte89"
+    }
+]
+
+
+//shows all reservations
+app.get("/api", function(req, res) {
+    return res.json(reservations);
+});
+
+
 app.listen(PORT, function() {
     console.log("App listening on PORT " + PORT);
 })
